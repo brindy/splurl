4245332bacd029591085e2fdeedb6e150dcbdd19
Brindy <brindy@duckduckgo.com>
2020-07-02 02:05:04 +0100

    update about

--- SplURL/ContentView.swift
+++ SplURL/ContentView.swift
@@ -37,8 +37,20 @@ struct AboutView: View {
 
     var body: some View {
         VStack {
-            Text("About 👀").font(.title)
+
+            Text("SplURL - the URL splitter").font(.title)
+
+            Image(systemName: "link").padding(48)
+
+            Text("(c) 2020 Brindy").padding()
+
+            Button("https://github.com/brindy/splurl") {
+                UIApplication.shared.open(URL(string: "https://github.com/brindy/splurl")!)
+            }
+
             Spacer()
+
+            Text("Inspired by Russell Holt").padding()
         }
     }
 
